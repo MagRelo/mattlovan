3b4b0d90bab0fc3d2790db8249683ae1c72b90e7
Matt Lovan <magrelo404@gmail.com>
2018-11-10 16:44:01 -0500

    rough out homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="container">
+          <header>
+            <h1>Servesa</h1>
+            <p>playground and demos by Matt Lovan</p>
+          </header>
+
+          <div className="grid">
+            <section id="contact">
+              <h2>Contact</h2>
+              <p>@mattlovan</p>
+              <p>medium</p>
+            </section>
+
+            <section id="demo-1">
+              <h2>#1</h2>
+            </section>
+
+            <section id="demo-2">
+              <h2>#2</h2>
+            </section>
+
+            <section id="demo-3">
+              <h2>#3</h2>
+            </section>
+
+            <section id="demo-4">
+              <h2>#4</h2>
+            </section>
+
+            <section id="timeline">
+              <h2>Timeline</h2>
+              <ul>
+                <li>0</li>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+              </ul>
+            </section>
+
+            <section id="writing">
+              <h2>Writing</h2>
+              <ul>
+                <li>article 1</li>
+                <li>article 2</li>
+                <li>article 3</li>
+              </ul>
+            </section>
+          </div>
+
+          <footer>
+            <p>2018</p>
+          </footer>
+        </div>
       </div>
     );
   }
